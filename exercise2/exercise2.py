cc7df5ba3e9de2ea039ba492ac807ddb7625ceb4
salmawagdy <salmawagdy227@gmail.com>
2025-11-06 13:30:31 +0200

    organize folders

--- exercise2/exercise2.py
+++ exercise2/exercise2.py
@@ -3,16 +3,3 @@ print (songs[0])
 print (songs[1])
 print (songs[2])
 print (songs[3])
-
-songs.sort()
-print (songs[0])
-print (songs[1])
-print (songs[2])
-print (songs[3])
-
-songs.append("melody")
-print ("there are "+str(len(songs))+" in the array")
-
-songs.pop(2)
-print(songs)
-print(len(songs))
