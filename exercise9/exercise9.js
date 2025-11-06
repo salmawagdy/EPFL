3eb6402f10fa2011fc8d189e27cbeec5ec1c381f
salmawagdy <salmawagdy227@gmail.com>
2025-11-06 14:37:45 +0200

    exercise9

---
+++ exercise9/exercise9.js
@@ -0,0 +1,7 @@
+let favoriteDishes = ["Pizza", "Pasta", "Sushi"];
+
+console.log("Here are my favorite dishes:");
+for (let i = 0; i < favoriteDishes.length; i++) {
+    console.log(favoriteDishes[i]);
+}
+console.log("What are your favorite dishes?");
