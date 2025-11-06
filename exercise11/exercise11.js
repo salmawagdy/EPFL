5ce3eabe509ecc63bc00f0fdd3a79359578cdc1f
salmawagdy <salmawagdy227@gmail.com>
2025-11-06 14:49:37 +0200

    exercise11

---
+++ exercise11/exercise11.js
@@ -0,0 +1,9 @@
+const businessCard = {
+first_name: "Salma",
+last_name: "Abdulmoniem",
+job_title: "Backend Developer",
+city: "Cairo",
+};
+
+console.log(businessCard.first_name);      
+console.log(businessCard["job_title"]);    
