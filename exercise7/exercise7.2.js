407dc46daaa5163de9c83f497a5a526f5e5cfc31
salmawagdy <salmawagdy227@gmail.com>
2025-11-06 13:47:29 +0200

    exercise7

---
+++ exercise7/exercise7.2.js
@@ -0,0 +1,10 @@
+let i = 1;
+while (i <= 3) {
+    if (i === 1) {
+        console.log(i + " step");
+    } else {
+        console.log(i + " steps");
+    }
+    i++;
+}
+console.log("Done!");
---
+++ exercise7/exercise7.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 3; i++) {
+    if (i === 1) {
+        console.log(i + " step");
+    } else {
+        console.log(i + " steps");
+    }
+}
+console.log("Done!");
