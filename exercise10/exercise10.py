138cb0a91550a87577dd7ce8b74cd2b7d07e9570
salmawagdy <salmawagdy227@gmail.com>
2025-11-06 14:43:23 +0200

    exercise10

---
+++ exercise10/exercise10.py
@@ -0,0 +1,10 @@
+business_card = {
+    "first_name": "Salma",
+    "last_name": "Abdulmoniem",
+    "job_title": "Backend Developer",
+    "city": "Cairo",
+}
+
+print(business_card["first_name"])
+print(business_card["job_title"])
+print(business_card["city"])
